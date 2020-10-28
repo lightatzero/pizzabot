20e41669c64cc3da11c6474da4cefc4db7c2d82a
Light <lightatzero@gmail.com>
2020-10-28 19:39:43 +0000

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='pizzabot',
-    version='0.1.1',
+    version='1.0.0',
     author='light at zero',
     author_email='lightatzero@gmail.com',
     packages=['pizzabot'],
