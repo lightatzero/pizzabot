3d16cb75982b1b8d5315fec500513b2906748bde
Light <lightatzero@gmail.com>
2020-10-30 23:01:27 +0000

    bug fix single coordinate would not work

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='pizzabot',
-    version='1.1.1',
+    version='1.1.3',
     author='light at zero',
     author_email='lightatzero@gmail.com',
     packages=['pizzabot'],
